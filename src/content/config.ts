72d0583b4ca28fb2334d7c986d6951d0e7a679f9
avearistov <chantepkik@gmail.com>
2024-06-22 11:40:16 +0700

    refactor: prettier format

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { defineCollection, z } from "astro:content";
 
 const articleSchema = z.object({
-	author: z.string(),
-	categories: z.array(z.string()),
-	date: z.string(),
-	featured: z.boolean(),
-	image: z.string(),
-	title: z.string(),
+  author: z.string(),
+  categories: z.array(z.string()),
+  date: z.string(),
+  featured: z.boolean(),
+  image: z.string(),
+  title: z.string(),
 });
 
 const articlesCollection = defineCollection({
-	schema: articleSchema,
+  schema: articleSchema,
 });
 
 export const collections = {
-	articles: articlesCollection,
+  articles: articlesCollection,
 };
