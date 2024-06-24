9882f61f5ea2a4b3b33d22d13b10bc7fae13258b
Ave Aristov <chantepkik@gmail.com>
2024-06-24 16:29:59 +0700

    feat: add basic card compoment + clean up code

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { languages, type SupportedLanguage } from "@/i18n/ui";
 import { navigate } from "astro:transitions/client";
 
@@ -10,15 +8,11 @@ const LanguageSwitcher = ({
   currentPath: string;
   initialLocale: SupportedLanguage;
 }) => {
-  const [currentLocale, setCurrentLocale] =
-    useState<SupportedLanguage>(initialLocale);
-
   const toggleLanguage = () => {
     const locales = Object.keys(languages) as SupportedLanguage[];
-    const currentIndex = locales.indexOf(currentLocale);
+    const currentIndex = locales.indexOf(initialLocale);
     const nextIndex = (currentIndex + 1) % locales.length;
     const newLocale = locales[nextIndex];
-    setCurrentLocale(newLocale);
 
     const newPath = `/${newLocale}${currentPath.replace(/^\/[a-z]{2}/, "")}`;
     navigate(newPath);
@@ -30,7 +24,7 @@ const LanguageSwitcher = ({
       className="text-foreground hover:text-primary"
       type="button"
     >
-      {languages[currentLocale]}
+      {languages[initialLocale]}
     </button>
   );
 };
