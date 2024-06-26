011005da87baf13eaf9d54498e2d33da557781f2
avearistov <chantepkik@gmail.com>
2024-06-26 17:44:58 +0700

    feat: improve html layout

--- src/components/WhatWeDoCard.tsx
+++ src/components/WhatWeDoCard.tsx
@@ -7,17 +7,19 @@ interface WhatWeDoCardProps {
 }
 
 const WhatWeDoCard = ({ title, description }: WhatWeDoCardProps) => (
-  <div className="flex flex-col items-start gap-2 rounded-lg border bg-card p-5 text-card-foreground">
+  <article className="flex flex-col items-start gap-2 rounded-lg border bg-card p-5 text-card-foreground">
     <ClipboardList size={37} className="text-primary" />
     <div>
-      <TextElement variant="smallheading" className="text-primary">
-        {title}
-      </TextElement>
+      <header>
+        <TextElement variant="smallheading" className="text-primary">
+          {title}
+        </TextElement>
+      </header>
       <TextElement variant="body" className="line-clamp-4 text-pretty">
         {description}
       </TextElement>
     </div>
-  </div>
+  </article>
 );
 
 export default WhatWeDoCard;
