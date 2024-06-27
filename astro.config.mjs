4f64e5a2a9c77df8e788a5c50d28a3c19e4a4dda
avearistov <chantepkik@gmail.com>
2024-06-27 19:47:20 +0700

    refactor : env var on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
 
-// Load environment variables
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-const PUBLIC_SITE_URL = env.PUBLIC_SITE_URL;
-
+console.log(process.env.PUBLIC_SITE_URL);
 // https://astro.build/config
 export default defineConfig({
-  site: PUBLIC_SITE_URL || "http://localhost:3000",
+  site: process.env.PUBLIC_SITE_URL || " http://localhost:4321",
   integrations: [
     react(),
     tailwind({
--- src/components/WhatWeDoGird.tsx
+++ src/components/WhatWeDoGird.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import WhatWeDoCard from "./WhatWeDoCard";
 
 const cards = [
