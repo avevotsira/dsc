1bf32e7bd3ee5e7f110e85a10a62654c622f8c83
Ave Aristov <chantepkik@gmail.com>
2024-07-01 12:47:47 +0700

    refactor: clearing console.log

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-console.log(process.env.PUBLIC_SITE_URL);
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || " http://localhost:4321",
