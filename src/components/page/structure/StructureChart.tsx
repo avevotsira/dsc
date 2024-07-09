510c47659f2c6d6f1e4b13bd377ce94a49065332
avearistov <chantepkik@gmail.com>
2024-07-09 20:34:41 +0700

    feat: optmize tailwind in chart

--- src/components/page/structure/StructureChart.tsx
+++ src/components/page/structure/StructureChart.tsx
@@ -6,14 +6,14 @@ const StructureChart = () => {
           Digital Security Committee
         </div>
         <div className="mb-8 grid gap-x-9 lg:grid-cols-3">
-          <div className="level-2 relative hidden max-w-xs rounded-md bg-sky-700 px-11 py-3 text-center text-primary-foreground before:absolute before:inset-y-0 before:left-full before:my-auto before:h-1 before:w-[4.5rem] before:bg-sky-300 sm:before:w-20 md:px-10 md:before:w-20 lg:inline-block lg:before:w-[5.5rem]">
+          <div className="level-2 relative hidden max-w-xs rounded-md bg-sky-700 px-11 py-3 text-center text-primary-foreground before:absolute before:inset-y-0 before:left-full before:my-auto before:h-1 before:w-[4.5rem] before:bg-sky-300 sm:before:w-20 md:px-10 lg:inline-block lg:before:w-[5.5rem]">
             Secretariat
           </div>
           <div className="level-2 relative inline-block max-w-xs rounded-md bg-primary px-10 py-3 text-center text-primary-foreground">
             Secretary General
           </div>
         </div>
-        <div className="relative grid grid-cols-4 lg:grid-cols-4 lg:gap-x-5 lg:gap-y-8 lg:px-0">
+        <div className="relative grid grid-cols-4 lg:gap-x-5 lg:gap-y-8">
           <div className="col-span-4 grid grid-cols-2 lg:hidden">
             <div className="border-l-4 border-t-4 border-sky-300 pb-3" />
           </div>
@@ -32,19 +32,19 @@ const StructureChart = () => {
           <div className="flex h-full items-center border-l-4 border-sky-300 lg:hidden">
             <div className="my-auto h-1 w-full bg-sky-300" />
           </div>
-          <div className="level-3 sm:after:w-54 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 after:bg-sky-300 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:left-1/2 after:lg:w-44">
+          <div className="level-3 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 after:bg-sky-300 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:left-1/2 lg:after:w-44">
             Cyber Security Cambodia
           </div>
           <div className="flex h-full items-center border-l-4 border-sky-300 lg:hidden">
             <div className="my-auto h-1 w-full bg-sky-300" />
           </div>
-          <div className="level-3 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:right-1/2 lg:after:w-44 lg:after:bg-sky-300">
+          <div className="level-3 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:w-44 lg:after:bg-sky-300">
             Cyber Security Cambodia
           </div>
           <div className="flex h-full items-center border-l-4 border-sky-300 lg:hidden">
             <div className="my-auto h-1 w-full bg-sky-300" />
           </div>
-          <div className="level-3 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 after:w-44 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:right-1/2 lg:after:w-80 lg:after:bg-sky-300">
+          <div className="level-3 relative col-span-3 my-4 flex max-w-xs items-center justify-center rounded-md bg-sky-700 px-2 py-3 text-center text-primary-foreground before:absolute before:inset-x-0 before:bottom-full before:mx-auto before:w-1 before:bg-sky-300 after:absolute after:-top-8 after:bottom-full after:right-1/2 after:h-1 after:w-44 sm:w-52 md:px-5 lg:col-span-1 lg:my-0 lg:w-auto lg:before:h-8 lg:after:w-80 lg:after:bg-sky-300">
             Cyber Security Cambodia
           </div>
         </div>
