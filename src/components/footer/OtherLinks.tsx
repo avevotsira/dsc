721a33d0feaee2f0ebea3fad1b3d1dcf79ddd45a
Hikaru <hikaru.uchita@gmail.com>
2024-07-09 11:31:58 +0700

    feat: const external link

--- src/components/footer/OtherLinks.tsx
+++ src/components/footer/OtherLinks.tsx
@@ -1,29 +1,37 @@
 import { ExternalLink } from "lucide-react";
 
 export default function OtherLinks() {
+  const ExternalLinks = [
+    {
+      label: "Ministry of Post and Telecommunications",
+      link: "https://mptc.gov.kh",
+    },
+    {
+      label: "Digital Economy And Business Committee",
+      link: "https://digitaleconomy.gov.kh",
+    },
+    { label: "Digital Government Committee", link: "https://dgc.gov.kh" },
+  ];
+
   return (
     <nav>
       <h2 className="mb-4 text-lg font-semibold">Other Links</h2>
       <ul className="space-y-4">
-        {[
-          "Ministry of Post and Telecommunications",
-          "Digital Economy And Business Committee",
-          "Digital Government Committee",
-        ].map((item) => (
-          <li key={item}>
+        {ExternalLinks.map((item) => (
+          <li key={item.label}>
             <a
-              href="/"
+              href={item.link}
               target="_blank"
               rel="noopener noreferrer"
               className="group flex items-start gap-2 hover:underline"
-              aria-label={`${item} (opens in a new tab)`}
+              aria-label={`${item.label} (opens in a new tab)`}
             >
               <ExternalLink
                 size={20}
                 className="mt-1 shrink-0 transition-colors group-hover:text-secondary"
                 aria-hidden="true"
               />
-              <span>{item}</span>
+              <span>{item.label}</span>
             </a>
           </li>
         ))}
