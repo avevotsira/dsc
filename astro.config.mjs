6cd68b2173110af4121815699e08fc631bc9a033
Ave Aristov <chantepkik@gmail.com>
2024-07-12 15:42:42 +0700

    feat: add site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || " http://localhost:4321",
+  site: process.env.PUBLIC_SITE_URL || "http://localhost:4321",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          km: "km-KH",
+        },
+      },
     }),
   ],
   i18n: {
