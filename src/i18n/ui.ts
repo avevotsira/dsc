2a3fb3eae44b13418231a18328fd8c6377db452d
Boti <s.visethboti.gmail.com>
2024-09-06 13:05:32 +0700

    feat: add english to khmer translation temp

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -4,19 +4,19 @@ export const ui: Record<string, Record<string, string>> = {
     "nav.about": "About",
     "nav.projects": "Projects",
     "home.whatwedo": "What We Do",
-    "structure.heading": "Organization Structure of DSC",
+    "structure.heading": "Organization Structure of D.S.C.",
     "structure.intro":
       "The Royal Government has established the Digital Security Committee (D.S.C.) in order to ensure the efficient and effective function in leading, coordinating and promoting the management of digital security to protect the interests of all social actors against all forms of attacks as to respond to all technical aspects and relevant forces and thereby maintaining and protecting digital security.",
     "structure.dscHeading":
       "Structure of the Digital Security Committee (D.S.C.)",
     "structure.dscIntro":
-      "The Digital Security Committee (DSC) of Cambodia is composed of high-ranking officials including the Prime Minister as Chairman, Deputy Prime Ministers, and various Ministers as Vice-Chairmen, along with Secretaries of State and military leaders as members. Its composition is formalized by the Royal Government, with provisions for adding more members as needed. The DSC convenes biannually, with the possibility of extraordinary meetings, chaired by designated officials or their delegates, ensuring continuity and decision-making authority across its membership.",
+      "The Digital Security Committee (D.S.C.) of Cambodia is composed of high-ranking officials including the Prime Minister as Chairman, Deputy Prime Ministers, and various Ministers as Vice-Chairmen, along with Secretaries of State and military leaders as members. Its composition is formalized by the Royal Government, with provisions for adding more members as needed. The DSC convenes biannually, with the possibility of extraordinary meetings, chaired by designated officials or their delegates, ensuring continuity and decision-making authority across its membership.",
     "structure.orgIntro":
       "The D.S.C. has a subordinating General Secretariat, overseeing 4 units, Cambodian Cyber Security Unit, Anti-Cybercrime Unit, Cyber Space Defense Unit, and Cyber Diplomatic affairs Unit. These units represent various ministries including Interior, National Defense, and Foreign Affairs, with provisions for additional deputies as needed. Appointments to these roles are made by the Royal Government upon request from the Minister of Posts and Telecommunications.",
     "structure.generalSecretaryHeading":
       "Structure of D.S.C.'s General Secretariat",
     "structure.generalSecretaryIntro":
-      "The General Secretary of the Digital Security Committee (DSC) is headed by the Minister of Posts and Telecommunications, supported by several Deputy Secretary Generals and is responsible for directs and coordinates all technical and administrative aspects of digital security, serving as a key assistant to the Chairman. This includes managing various specialized units, overseeing human resources and budgeting, ensuring secure file and data management, and preparing comprehensive reports on digital security efforts. Additionally, they undertake any further duties as required by the Chairman, showcasing their pivotal role in the operational and strategic functions of the DSC.",
+      "The General Secretary of the Digital Security Committee (D.S.C.) is headed by the Minister of Posts and Telecommunications, supported by several Deputy Secretary Generals and is responsible for directs and coordinates all technical and administrative aspects of digital security, serving as a key assistant to the Chairman. This includes managing various specialized units, overseeing human resources and budgeting, ensuring secure file and data management, and preparing comprehensive reports on digital security efforts. Additionally, they undertake any further duties as required by the Chairman, showcasing their pivotal role in the operational and strategic functions of the DSC.",
     "about.dsc": "About Digital Security Committee",
     "about.dscText1":
       "In 2021, to ensure the development of vibrant digital economy and society of Cambodia, the Royal Government of Cambodia under the leadership of Samdech Akka Moha Sena Padei Techo Hun Sen, has established the “National Digital Economy and Society Council” in accordance with Cambodia Digital Economy and Society Policy Framework 2021 – 2035. ",
@@ -32,7 +32,7 @@ export const ui: Record<string, Record<string, string>> = {
       "The Digital Security Committee has established and outlined the following strategies",
     "about.roleResponsibilities": "Role and Responsibilites",
     "about.roleResponsibilitiesText":
-      "The Digital Security Committee (DSC) was established under Sub-Decree No. 44 ANKR.BT on the Organization and Functioning of Digital Security Committee dated 1st May, 2024 with role and responsibilities:",
+      "The Digital Security Committee (D.S.C.) was established under Sub-Decree No. 44 ANKR.BT on the Organization and Functioning of Digital Security Committee dated 1st May, 2024 with role and responsibilities:",
     "unit.heading": "Units Under the Digital Security Committee",
     "unit.headingText":
       "The Digital Security Committee (D.S.C.) is organized around a key General Secretariat that oversees four essential units: the Cambodian Cyber Security Unit, Anti-Cybercrime Unit, Cyber Space Defense Unit, and Cyber Diplomatic Affairs Unit.",
@@ -50,19 +50,54 @@ export const ui: Record<string, Record<string, string>> = {
       "The Unit in charge of cyber diplomacy under the jurisdiction of the Ministry of Foreign Affairs and International Cooperation shall perform the additional function as the Cyber Diplomatic Unit, and shall serve as the assistant for the General Secretariat of the D.S.C. The Cyber Diplomatic Unit shall fulfill the roles and duties as set out in the legal instruments related to digital security management",
   },
   km: {
-    "nav.home": "Accueil",
-    "nav.about": "À propos",
-    "home.whatwedo": "កាតព្វកិច្ចរបស់យើង",
-    "structure.heading": "រចនាសម្ព័ន្ធរបស់អគ្គលេខាធិការដ្ឋាន គ.ស.ឌ.",
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.projects": "Projects",
+    "home.whatwedo": "What We Do",
+    "structure.heading": "Organization Structure of D.S.C.",
     "structure.intro":
-      "រាជរដ្ឋាភិបាលបានបង្កើត “គណៈកម្មាធិការសន្តិសុខឌីជីថល” ដែលហៅកាត់ថា គ.ស.ឌ. ក្នុងគោលបំណងធានាប្រសិទ្ធភាពនិងស័ក្តិសិទ្ធិភាពនៃការបំពេញមុខងារដឹកនាំ សម្របសម្រួល និងជំរុញការគ្រប់គ្រងសន្តិសុខឌីជីថល ដើម្បីការពារអត្ថប្រយោជន៍របស់គ្រប់តួអង្គសង្គមទប់ទល់នឹងការវាយប្រហារគ្រប់រូបភាព ឆ្លើយតបគ្រប់ផ្នែកទាំងបច្ចេកទេសនិងកម្លាំងពាក់ព័ន្ធនឹងការធានានិងការពារសន្តិសុខឌីជីថល។ ",
+      "The Royal Government has established the Digital Security Committee (D.S.C.) in order to ensure the efficient and effective function in leading, coordinating and promoting the management of digital security to protect the interests of all social actors against all forms of attacks as to respond to all technical aspects and relevant forces and thereby maintaining and protecting digital security.",
+    "structure.dscHeading":
+      "Structure of the Digital Security Committee (D.S.C.)",
+    "structure.dscIntro":
+      "The Digital Security Committee (D.S.C.) of Cambodia is composed of high-ranking officials including the Prime Minister as Chairman, Deputy Prime Ministers, and various Ministers as Vice-Chairmen, along with Secretaries of State and military leaders as members. Its composition is formalized by the Royal Government, with provisions for adding more members as needed. The DSC convenes biannually, with the possibility of extraordinary meetings, chaired by designated officials or their delegates, ensuring continuity and decision-making authority across its membership.",
+    "structure.orgIntro":
+      "The D.S.C. has a subordinating General Secretariat, overseeing 4 units, Cambodian Cyber Security Unit, Anti-Cybercrime Unit, Cyber Space Defense Unit, and Cyber Diplomatic affairs Unit. These units represent various ministries including Interior, National Defense, and Foreign Affairs, with provisions for additional deputies as needed. Appointments to these roles are made by the Royal Government upon request from the Minister of Posts and Telecommunications.",
+    "structure.generalSecretaryHeading":
+      "Structure of D.S.C.'s General Secretariat",
+    "structure.generalSecretaryIntro":
+      "The General Secretary of the Digital Security Committee (D.S.C.) is headed by the Minister of Posts and Telecommunications, supported by several Deputy Secretary Generals and is responsible for directs and coordinates all technical and administrative aspects of digital security, serving as a key assistant to the Chairman. This includes managing various specialized units, overseeing human resources and budgeting, ensuring secure file and data management, and preparing comprehensive reports on digital security efforts. Additionally, they undertake any further duties as required by the Chairman, showcasing their pivotal role in the operational and strategic functions of the DSC.",
     "about.dsc": "About Digital Security Committee",
-    "about.dscText":
-      "រាជរដ្ឋាភិបាលបានបង្កើត “គណៈកម្មាធិការសន្តិសុខឌីជីថល” ដែលហៅកាត់ថា គ.ស.ឌ. ក្នុងគោលបំណងធានាប្រសិទ្ធភាពនិងស័ក្តិសិទ្ធិភាពនៃការបំពេញមុខងារដឹកនាំ សម្របសម្រួល និងជំរុញការគ្រប់គ្រងសន្តិសុខឌីជីថល ដើម្បីការពារអត្ថប្រយោជន៍របស់គ្រប់តួអង្គសង្គមទប់ទល់នឹងការវាយប្រហារគ្រប់រូបភាព ឆ្លើយតបគ្រប់ផ្នែកទាំងបច្ចេកទេសនិងកម្លាំងពាក់ព័ន្ធនឹងការធានានិងការពារសន្តិសុខឌីជីថល។ ",
-    "about.strategies": "គោលដៅជាយុទ្ធសាស្រ្ត និងយុទ្ធសាស្ត្រ",
+    "about.dscText1":
+      "In 2021, to ensure the development of vibrant digital economy and society of Cambodia, the Royal Government of Cambodia under the leadership of Samdech Akka Moha Sena Padei Techo Hun Sen, has established the “National Digital Economy and Society Council” in accordance with Cambodia Digital Economy and Society Policy Framework 2021 – 2035. ",
+    "about.dscText2":
+      "According to Cambodia Digital Economy and Society Policy Framework 2021 – 2035, the National Digital Economy and Society Council has three “Etat-Major” committees that are responsible for specific technical tasks. The committees are:",
+    "about.dscText3":
+      "Throughout the process of digital transformation and implementation of Cambodia Digital Economy and Society Policy Framework 2021 – 2035, the Royal Government of Cambodia has foreseen “Digital Security” as a vital asset to ensure the successful implementation of the framework itself and also to ensure that Cambodia’s cyberspace is safe from cyber risk. Therefore, in 2024, The Royal Government of Cambodia, under the leadership of Samdech Moha Borvor Thipadei Hun Manet, has established the “Digital Security Committee” which is the technical committee responsible for managing Cambodia’s digital security with the purpose of protecting the interest of users, resisting cyber-attack and competent to respond to all areas, including technical and strength, as well as the management of the entire national social security.",
+    "about.goals": "Goals",
+    "about.goalsText":
+      "In order to achieve the above vision and objectives, the Royal Government has set four strategic goals as follows:",
+    "about.strategies": "Strategies",
     "about.strategiesText":
-      "ដើម្បីសម្រេចបាននូវចក្ខុវិស័យនិងគោលបំណងខាងលើ រដ្ឋាភិបាលដាក់ចេញគោលដៅជាយុទ្ធសាស្ត្រចំនួន៤និងយុទ្ធសាស្ត្រចំនួន១០ ដូចខាងក្រោម៖",
-    "about.roleResponsibilities": "តួនាទី និងភារកិច្ច",
+      "The Digital Security Committee has established and outlined the following strategies",
+    "about.roleResponsibilities": "Role and Responsibilites",
+    "about.roleResponsibilitiesText":
+      "The Digital Security Committee (D.S.C.) was established under Sub-Decree No. 44 ANKR.BT on the Organization and Functioning of Digital Security Committee dated 1st May, 2024 with role and responsibilities:",
+    "unit.heading": "Units Under the Digital Security Committee",
+    "unit.headingText":
+      "The Digital Security Committee (D.S.C.) is organized around a key General Secretariat that oversees four essential units: the Cambodian Cyber Security Unit, Anti-Cybercrime Unit, Cyber Space Defense Unit, and Cyber Diplomatic Affairs Unit.",
+    "unit.ccsu": "Cambodia Cyber Security Unit",
+    "unit.ccsuText":
+      "The Cambodia Cyber Security Unit that is under the jurisdiction of the Ministry of Post and Telecommunications, and shall serve as the assistant to the General Secretariat of the D.S.C. on cybersecurity. The organization and functioning of the Cambodian Cyber Security Unit shall be determined by a separate Sub-decree at the request of the Minister of the MPTC.",
+    "unit.cybercrime": "Anti-Cybercrime Unit",
+    "unit.cybercrimeText":
+      "The Unit in charge of research, investigation and suppression of cybercrime that is under the jurisdiction of the Ministry of Interior shall perform the additional function of the Anti-Cybercrime Unit and shall serve asthe assistant for the General Secretariat of the D.S.C. The Anti-cybercrime Unit shall fulfill the roles and duties as set out in the legal instruments related to digital security management.",
+    "unit.defense": "Cyber Defense Unit",
+    "unit.defenseText":
+      "The Unit in charge of the National Cyberspace Defense under the jurisdiction of the Ministry of the National Defense shall perform additional functions as the National Cyberspace Defense Unit and shall serve as the assistant for the General Secretariat of the D.S.C. The National Cyberspace Defense Unit shall fulfill the roles and duties as set out in the legal instruments related to digital security management.",
+    "unit.diplomatic": "Cyber Diplomatic Unit",
+    "unit.diplomaticText":
+      "The Unit in charge of cyber diplomacy under the jurisdiction of the Ministry of Foreign Affairs and International Cooperation shall perform the additional function as the Cyber Diplomatic Unit, and shall serve as the assistant for the General Secretariat of the D.S.C. The Cyber Diplomatic Unit shall fulfill the roles and duties as set out in the legal instruments related to digital security management",
   },
 } as const;
 
