0ac9a704c0fa284be95f82ac69a8154159964b07
Boti <s.visethboti.gmail.com>
2024-09-12 11:35:42 +0700

    feat: remove buttonlink to camcert, fix typo cyber space

--- src/components/header/MobileNavMenu.tsx
+++ src/components/header/MobileNavMenu.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/sheet";
 
 import { ButtonLink } from "../ButtonLink";
+import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import { Menu } from "lucide-react";
 
@@ -76,13 +77,7 @@ const NavMenu: React.FC<{ navMenuItem: NavMenuProps[] }> = ({
           ))}
           <Separator />
           <li className="flex justify-center pt-4">
-            <ButtonLink
-              href="https://www.camcert.gov.kh/en/report-incident/"
-              target="_blank"
-              className="lg:hidden"
-            >
-              Report Incident
-            </ButtonLink>
+            <Button className="lg:hidden">Report Incident</Button>
           </li>
         </ul>
       </SheetContent>
